e08a3d5d98963b19a1a8192796110f71430d1e78
AS <asadrgilany7@gmail.com>
2022-01-18 12:11:57 -0500

    initial resolvers complete

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,79 @@
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+	Query: {
+		me: async (parent, args, context) => {
+			if (context.user) {
+				const userData = await User.findOne({
+					_id: context.user._id
+				})
+					.select("-__v -password")
+					.populate("savedItems");
+
+				return userData;
+			}
+			throw new AuthenticationError("You must be logged in!");
+		}
+	},
+	Mutation: {
+		login: async (parent, { email, password }) => {
+			const user = await User.findOne({ email });
+			if (!user) {
+				throw new AuthenticationError("Invalid credentials!");
+			}
+
+			const correctPw = await user.isCorrectPassword(password);
+			if (!correctPw) {
+				throw new AuthenticationError("Invalid credentials!");
+			}
+
+			const token = signToken(user);
+			return { token, user };
+		},
+		addUser: async (parent, args) => {
+			const user = await User.create(args);
+			const token = signToken(user);
+			return { token, user };
+		},
+		saveItem: async (parent, args, context) => {
+			if (context.user) {
+				const user = await User.findByIdAndUpdate(
+					{
+						_id: context.user._id
+					},
+					{
+						$addToSet: { savedItems: args.itemData }
+					},
+					{
+						new: true,
+						runValidators: true
+					}
+				);
+				return user;
+			}
+			throw new AuthenticationError("You must be logged in!");
+		},
+		removeItem: async (parent, args, context) => {
+			if (context.user) {
+				const user = await User.findByIdAndUpdate(
+					{
+						_id: context.user._id
+					},
+					{
+						$pull: { savedItems: args.itemId }
+					},
+					{
+						new: true,
+						runValidators: true
+					}
+				);
+				return user;
+			}
+			throw new AuthenticationError("You must be logged in!");
+		}
+	}
+};
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,41 +1,46 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID
-        username: String
-        email: String
-        savedItems: [Item]
-    }
+	type User {
+		_id: ID
+		username: String
+		email: String
+		savedItems: [Item]
+	}
 
-    type Item {
-        itemId: ID
-        name: String
-        description: String
-        image: String
-        price: Int
-        link: String
-    }
+	type Item {
+		itemId: ID
+		name: String
+		description: String
+		image: String
+		price: Int
+		link: String
+	}
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+	type Auth {
+		token: ID!
+		user: User
+	}
 
-    type Query {
-        me: User
-    }
+	type Query {
+		me: User
+	}
 
-    input itemInput {
-        itemId: String
-        name: String
-        description: String
-        image: String
-        price: Int
-        link: String
-    }
+	input itemInput {
+		itemId: String
+		name: String
+		description: String
+		image: String
+		price: Int
+		link: String
+	}
 
-    type Mutation {
-        login(email: String!, password: String!)
-    }
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		saveItem(itemData: itemInput!): User
+		removeItem(itemId: ID!): User
+	}
 `;
+
+module.exports = typeDefs;
