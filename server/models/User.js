7a98c8396c7a7d67fe385878fdc7f86095fd36db
AS <asadrgilany7@gmail.com>
2022-01-18 11:15:02 -0500

    added auth functionality

---
+++ server/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const { Schema } = require("mongoose");
+const bcrypt = require("bcrypt");
+import isEmail from "validator/lib/isEmail";
+import isLength from "validator/lib/isLength";
+
+const userSchema = new Schema({
+	username: {
+		type: String,
+		unique: true,
+		require: [true, "A username is required!"],
+		trim: true
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: [true, "An email is required!"],
+		validate: [isEmail, "Please enter a valid email address!"]
+	},
+	password: {
+		type: String,
+		required: true,
+		minlength: 8,
+		validate: [
+			isLength(password, 8),
+			"Password must be at least 8 characters!"
+		]
+	}
+});
+
+// set up pre-save middleware to create password
+userSchema.pre("save", async function (next) {
+	if (this.isNew || this.isModified("password")) {
+		const saltRounds = 10;
+		this.password = await bcrypt.hash(this.password, saltRounds);
+	}
+
+	next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+	return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
