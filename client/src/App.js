83978a597b12e8352c1e0d8bc155b8f2d5812d14
AS <asadrgilany7@gmail.com>
2022-01-18 15:38:19 -0500

    working on getting initial front end structure in place

--- client/src/App.js
+++ client/src/App.js
@@ -1,58 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Provider } from "react-redux";
+import createStore from "./store";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+const httpLink = createHttpLink({
+	uri: "/graphql"
+});
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem("id_token");
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : ""
+		}
+	};
+});
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache()
+});
+const store = createStore;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<div>
+					<Provider store={store}>
+						<Nav />
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/signup" element={<Signup />} />
+
+							<Route element={<NoMatch />} />
+						</Routes>
+					</Provider>
+				</div>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
