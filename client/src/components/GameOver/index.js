076a7ec7b14bffa3aef1273e61ce7f991226d61d
asadg7 <asadrgilany7@gmail.com>
2022-01-31 09:20:35 -0500

    adding to game over page

--- client/src/components/GameOver/index.js
+++ client/src/components/GameOver/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import { useStoreContext } from "../../utils/GlobalState";
 import { Heading, Text } from "@chakra-ui/core";
+import Auth from "../../utils/auth";
 
 function GameOver() {
     const [state] = useStoreContext();
+
+    const saveScore = () => {
+        if (Auth.loggedIn()) {
+            return (
+                <button>Save Score!</button>
+            )
+        } else {
+            return (
+                <div>Sign up or Log in to save your high score!</div>
+            )
+        }
+    }
 
     return (
         <div>
@@ -13,6 +26,7 @@ function GameOver() {
             <Text color="black">
                 {`Your final score: ${state.score}`}
             </Text>
+            {saveScore()}
         </div>
     );
 };
